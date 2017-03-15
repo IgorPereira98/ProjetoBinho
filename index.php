44e0889716070832662bcb4bc5b2d8e9709419c7
IgorRafael <igor_rafael25@outlook.com>
2017-03-15 18:19:29 -0300

    Campo 1 criado

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
+
+
 <!DOCTYPE html>
 <html>
    <head>
-        <title></title>
+        <title>Projeto Binho</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="bootstrap.min.css" rel="stylesheet">
   </head>
-<body>
+    <body>
+         <form>
+          <form id="formulario" autocomplete="off">
+        <fieldset>
+          <legend>Formulário</legend>
+       <label>Campo1:</label><input class="campo_1" type="number"><br>
+   </form>
 
-</body>
+
+    </body>
+
 </html>
